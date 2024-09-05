8ccd846c8945a426bb42a8acf293be86800a5839
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-09-05 21:24:41 +0300

    fix

--- main.py
+++ main.py
@@ -6,13 +6,12 @@ def main():
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
     pygame.init()
-    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
-        surface.fill(BLACK)
+        screen.fill('black')
         pygame.display.flip()
 
 if __name__ == "__main__":
