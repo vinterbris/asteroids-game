b7e532912de27d2581c89bc7fb1850838cd5fdde
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-09-05 21:22:22 +0300

    displays black window and properly closes

--- main.py
+++ main.py
@@ -5,6 +5,15 @@ def main():
     print('Starting asteroids!')
     print(f'Screen width: {SCREEN_WIDTH}')
     print(f'Screen height: {SCREEN_HEIGHT}')
+    pygame.init()
+    pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        surface = pygame.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+        surface.fill(BLACK)
+        pygame.display.flip()
 
 if __name__ == "__main__":
     main()
