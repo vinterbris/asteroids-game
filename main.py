a56a9f4fe50a05c2b2c9311af81d9834e16fce06
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-09-06 12:02:32 +0300

    finished drawing player

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import pygame
 from constants import *
+from player import Player
+
 
 def main():
     print('Starting asteroids!')
@@ -10,12 +12,14 @@ def main():
 
     clock = pygame.time.Clock()
     dt = 0
+    player = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
         screen.fill('black')
+        player.draw(screen)
         pygame.display.flip()
         dt = clock.tick(60) / 1000
 
