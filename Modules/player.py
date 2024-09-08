c4356de08af5857fc1f45c47b00ee64684badab1
Sergey Dobrovolskiy <21102027+vinterbris@users.noreply.github.com>
2024-09-08 14:08:17 +0300

    timer refactor

--- Modules/player.py
+++ Modules/player.py
@@ -22,7 +22,7 @@ class Player(CircleShape):
     def __init__(self, x, y):
         super().__init__(x, y, PLAYER_RADIUS)
         self.rotation = 0
-        self.timer = 0
+        self.shoot_timer = 0
         self.max_speed = PLAYER_MAX_SPEED
         self.speed = PLAYER_SPEED
         self.acceleration = PLAYER_ACCELERATION
@@ -44,6 +44,9 @@ class Player(CircleShape):
         self.rotation += PLAYER_TURN_SPEED * dt
 
     def update(self, dt):
+        self.timer(dt)
+        print(self.shoot_timer)
+
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_a]:
@@ -55,22 +58,20 @@ class Player(CircleShape):
         if keys[pygame.K_s]:
             self.move(-dt)
         if keys[pygame.K_SPACE]:
-            if self.timer == 0:
-                self.shoot()
-                self.timer = PLAYER_SHOOT_COOLDOWN
-            else:
-                pass
+            self.shoot()
 
-        if self.timer > 0:
-            self.timer -= dt
-            if self.timer < 0:
-                self.timer = 0
+    def timer(self, dt):
+        if self.shoot_timer > 0:
+            self.shoot_timer -= dt
 
     def move(self, dt):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         self.position += forward * PLAYER_SPEED * dt
 
     def shoot(self):
+        if self.shoot_timer > 0:
+            return
+        self.shoot_timer = PLAYER_SHOOT_COOLDOWN
         shot = Shot(self.position.x, self.position.y)
         shot.velocity = pygame.Vector2(0, 1).rotate(self.rotation) * PLAYER_SHOOT_SPEED
 
